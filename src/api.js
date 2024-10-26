ebaa525b6a7604aa867c8adeec22edc007ec5f66
torres5271 <gustavo.souza@sptech.school>
2024-10-26 15:45:10 -0300

    teste imagem

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const api = axios.create({
-    baseURL: "http://44.219.104.121"
+    baseURL: "http://34.200.3.23"
 });
 
 export default api;
